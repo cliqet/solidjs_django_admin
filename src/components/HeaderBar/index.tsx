d667ddcef09236c673c4ff8d5c9a41146163a19f
Mike Torres <michael@aqwire.co>
2025-02-21 17:16:06 +0700

    make header responsive

--- src/components/HeaderBar/index.tsx
+++ src/components/HeaderBar/index.tsx
@@ -9,6 +9,7 @@ import {
 import { logoutUser } from "src/services/users";
 import MoonIcon from "src/assets/icons/moon-icon";
 import SunIcon from "src/assets/icons/sun-icon";
+import BurgerIcon from "src/assets/icons/burger-icon";
 
 const HeaderBar = () => {
   const [isProfileDropdownShowing, setIsProfileDropdownShowing] =
@@ -79,22 +80,10 @@ const HeaderBar = () => {
             class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
           >
             <span class="sr-only">Open sidebar</span>
-            <svg
-              class="w-6 h-6"
-              aria-hidden="true"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                clip-rule="evenodd"
-                fill-rule="evenodd"
-                d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
-              ></path>
-            </svg>
+            <BurgerIcon width={6} height={6} />
           </button>
 
-          <span class="ml-2 self-center text-xl font-semibold sm:text-2xl whitespace-nowrap text-white">
+          <span class="hidden md:block ml-2 self-center text-xl font-semibold sm:text-2xl whitespace-nowrap text-white">
             <A href={dashboardRoute(authRoute.dashboardHomeView)}>MainApp</A>
           </span>
         </div>
@@ -121,7 +110,7 @@ const HeaderBar = () => {
                 </A>
               </div>
 
-              <div>
+              <div class="hidden md:block">
                 <span class="text-white text-xs mr-2">
                   {appState.user?.email}
                 </span>

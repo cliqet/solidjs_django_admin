ccf5222c0c999e746c28eaa485d5ddea1f020a5f
Mike Torres <michael@aqwire.co>
2025-02-28 20:12:46 +0700

    modify settings page design

--- src/pages/SettingsPage/index.tsx
+++ src/pages/SettingsPage/index.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "@solidjs/router";
+import PostgresIcon from "src/assets/icons/postgres-icon";
 import RedisIcon from "src/assets/icons/redis-icon";
-import ReportsIcon from "src/assets/icons/reports-icon";
 import { authRoute, dashboardRoute } from "src/hooks/useAdminRoute";
 
 const SettingsPage = () => {
@@ -10,28 +10,28 @@ const SettingsPage = () => {
     <div class="flex-col justify-between p-1 items-center">
       <h1 class="text-xl dark:text-white mb-5">Settings</h1>
 
-      <div class="p-2 bg-custom-primary-lighter rounded-md flex-col mb-5">
+      <div class="p-2 bg-green-100 dark:bg-gray-700 rounded-md flex-col mb-5">
         <div class="flex items-center gap-2">
           <div class="w-8">
-            <RedisIcon class="w-7 h-7 text-white" />
+            <RedisIcon class="w-7 h-7 text-red-500" />
           </div>
           <h3
             onClick={() => navigate(dashboardRoute(authRoute.queuesView))}
-            class="text-white cursor-pointer hover:underline"
+            class="text-red-600 cursor-pointer hover:underline"
           >
             Queues
           </h3>
         </div>
       </div>
 
-      <div class="p-2 bg-custom-primary-lighter rounded-md flex-col mb-5">
+      <div class="p-2 bg-green-100 dark:bg-gray-700 rounded-md flex-col mb-5">
         <div class="flex items-center gap-2">
           <div class="w-8">
-            <ReportsIcon class="w-7 h-7 text-white" />
+            <PostgresIcon class="w-7 h-7 text-blue-500" />
           </div>
           <h3
             onClick={() => navigate(dashboardRoute(authRoute.queryReportsView))}
-            class="text-white cursor-pointer hover:underline"
+            class="text-blue-500 cursor-pointer hover:underline"
           >
             Query Reports
           </h3>
